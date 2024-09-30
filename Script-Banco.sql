724f10efd02b8b8214323948ceddace9c7eace23
MatheusFerroTorres <matheus.torres@sptech.school>
2024-09-30 17:59:12 -0300

    Alterando relacionamentos e adicionando outra modelagem

--- Script-Banco.sql
+++ Script-Banco.sql
@@ -9,8 +9,8 @@ CREATE TABLE Zona (
 CREATE TABLE Bairro (
     idBairro INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
-    idZona INT,
-    FOREIGN KEY (idZona) REFERENCES Zona(idZona)
+    fkZona INT,
+    FOREIGN KEY (fkZona) REFERENCES Zona(idZona)
 );
 
 CREATE TABLE Logradouro (
@@ -18,8 +18,8 @@ CREATE TABLE Logradouro (
     nome VARCHAR(100) NOT NULL,
     cep VARCHAR(9),
     numero VARCHAR(10),
-    idBairro INT,
-    FOREIGN KEY (idBairro) REFERENCES Bairro(idBairro)
+    fkBairro INT,
+    FOREIGN KEY (fkBairro) REFERENCES Bairro(idBairro)
 );
 
 CREATE TABLE Empresa (
@@ -27,8 +27,8 @@ CREATE TABLE Empresa (
     nome VARCHAR(100) NOT NULL, 
     cnpj CHAR(14),
     telefone CHAR(15),
-    idLogradouro INT,
-    FOREIGN KEY (idLogradouro) REFERENCES Logradouro(idLogradouro)
+    fkLogradouro INT,
+    FOREIGN KEY (fkLogradouro) REFERENCES Logradouro(idLogradouro)
 );
 
 CREATE TABLE Cargo (
@@ -41,8 +41,8 @@ CREATE TABLE Usuario (
     nome VARCHAR(45) NOT NULL,
     senha VARCHAR(60) NOT NULL, 
     telefone VARCHAR(15),        
-    idCargo INT,
-    FOREIGN KEY (idCargo) REFERENCES Cargo(idCargo)
+    fkCargo INT,
+    FOREIGN KEY (fkCargo) REFERENCES Cargo(idCargo)
 );
 
 CREATE TABLE Local (
@@ -55,57 +55,17 @@ CREATE TABLE Crime (
     natureza VARCHAR(45) NOT NULL,
     dataOcorrencia DATETIME NOT NULL,
     descricao VARCHAR(255),      
-    idLogradouro INT,
-    idLocal INT,
-    FOREIGN KEY (idLogradouro) REFERENCES Logradouro(idLogradouro),
-    FOREIGN KEY (idLocal) REFERENCES Local(idLocal)
+    fkLogradouro INT,
+    fkLocal INT,
+    FOREIGN KEY (fkLogradouro) REFERENCES Logradouro(idLogradouro),
+    FOREIGN KEY (fkLocal) REFERENCES Local(idLocal)
 );
 
 CREATE TABLE Lembrete (
-    idCrime VARCHAR(45),
-    idUsuario INT,
+    fkCrime VARCHAR(45),
+    fkUsuario INT,
     descricao TEXT,          
-    PRIMARY KEY (idCrime, idUsuario),
-    FOREIGN KEY (idCrime) REFERENCES Crime(idCrime),
-    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
-);
-
-
-CREATE TABLE Cargo (
-    idCargo INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45) NOT NULL
-);
-
-CREATE TABLE Usuario (
-    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45) NOT NULL,
-    senha VARCHAR(45) NOT NULL,
-    telefone VARCHAR(12),
-    idCargo INT,
-    FOREIGN KEY (idCargo) REFERENCES Cargo(idCargo)
-);
-
-CREATE TABLE Local (
-    idLocal INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45) NOT NULL
-);
-
-CREATE TABLE Crime (
-    idCrime VARCHAR(45) PRIMARY KEY,
-    natureza VARCHAR(45) NOT NULL,
-    dataOcorrencia DATETIME NOT NULL,
-    descricao VARCHAR(100),
-    idLogradouro INT,
-    idLocal INT,
-    FOREIGN KEY (idLogradouro) REFERENCES Logradouro(idLogradouro),
-    FOREIGN KEY (idLocal) REFERENCES Local(idLocal)
-);
-
-CREATE TABLE Lembrete (
-    idCrime VARCHAR(45),
-    idUsuario INT,
-    descricao TEXT(50),
-    PRIMARY KEY (idCrime, idUsuario),
-    FOREIGN KEY (idCrime) REFERENCES Crime(idCrime),
-    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+    PRIMARY KEY (fkCrime, fkUsuario),
+    FOREIGN KEY (fkCrime) REFERENCES Crime(idCrime),
+    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario)
 );
