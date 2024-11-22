9ee6a0ac14de7df6d5988ae706ddf9ab4bab2faa
Alejandro Castor <128645676+AlejandroFadim@users.noreply.github.com>
2024-11-22 18:02:28 -0300

    Script com as view

--- Script-Banco.sql
+++ Script-Banco.sql
@@ -76,3 +76,92 @@ VALUES
     ('biancaRodrigues@infotrack.com', 'Bianca Rodrigues', 'Bianca30', '11999933333', 3, 1),
     ('alejandroCastor@infotrack.com', 'Alejandro Castor', 'Alejandro40', '11999944444', 3, 1),
     ('cintiaOhara@infotrack.com', 'Cintia Ohara', 'Cintia50', '11999955555', 3, 1);
+
+CREATE VIEW TopBairrosCrimes AS
+SELECT 
+    b.nome AS Bairro, 
+    COUNT(c.idCrime) AS TotalCrimes
+FROM 
+    Bairro b
+JOIN 
+    Logradouro l ON b.idBairro = l.fkBairro
+JOIN 
+    Crime c ON l.idLogradouro = c.fkLogradouro
+GROUP BY 
+    b.idBairro, b.nome
+ORDER BY 
+    TotalCrimes DESC
+LIMIT 5;
+
+------------------------------------------------------------------------------
+
+CREATE VIEW TopRuasCrimes AS
+SELECT 
+    l.nome AS Rua, 
+    COUNT(c.idCrime) AS TotalCrimes
+FROM 
+    Logradouro l
+JOIN 
+    Crime c ON l.idLogradouro = c.fkLogradouro
+WHERE 
+    l.nome <> 'VEDAÇÃO DA DIVULGAÇÃO DOS DADOS RELATIVOS'
+GROUP BY 
+    l.idLogradouro, l.nome
+ORDER BY 
+    TotalCrimes DESC
+LIMIT 10;
+
+
+
+CREATE VIEW periodoCrime AS
+SELECT
+    CASE
+        WHEN EXTRACT(HOUR FROM dataOcorrencia) BETWEEN 0 AND 5 THEN 'Madrugada'
+        WHEN EXTRACT(HOUR FROM dataOcorrencia) BETWEEN 6 AND 10 THEN 'Manhã'
+        WHEN EXTRACT(HOUR FROM dataOcorrencia) BETWEEN 11 AND 11 THEN 'Tarde'
+        WHEN EXTRACT(HOUR FROM dataOcorrencia) BETWEEN 12 AND 23 THEN 'Noite'
+    END AS periodo,
+    COUNT(*) * 1.0 / COUNT(DISTINCT DATE(dataOcorrencia)) AS valores
+FROM Crime
+GROUP BY periodo
+ORDER BY FIELD(periodo, 'Madrugada', 'Manhã', 'Tarde', 'Noite');
+
+----------------------------------------------
+
+create view totalMeses as
+SELECT 
+    t1.tipoLocal as local,
+    t1.ano,
+    MONTHNAME(DATE(CONCAT(t1.ano, '-', t1.mes, '-01'))) AS mes,
+    SUM(t1.totalCrimes) AS totalOcorrencias
+FROM (
+    SELECT 
+        CASE 
+            WHEN L.nome IN ('Casa', 'Apartamento', 'Casas', 'Moradia', 'Garagem coletiva de prédio', 
+                             'Residência', 'Edícula/Fundos', 'Condomínio Residencial', 'Sítio', 'Chácara', 
+                             'Chácaras', 'Loteamento') THEN 'Residencial'
+            ELSE 'Comercial'
+        END AS tipoLocal,
+        YEAR(C.dataOcorrencia) AS ano,
+        MONTH(C.dataOcorrencia) AS mes,
+        COUNT(C.idCrime) AS totalCrimes
+    FROM 
+        Crime C
+    JOIN 
+        Local L ON C.fkLocal = L.idLocal
+    WHERE 
+        C.natureza IN ('Furto', 'Roubo')
+        AND YEAR(C.dataOcorrencia) = YEAR(CURDATE()) 
+        AND MONTH(C.dataOcorrencia) BETWEEN 1 AND 6  -- Meses de janeiro a junho
+    GROUP BY 
+        tipoLocal, YEAR(C.dataOcorrencia), MONTH(C.dataOcorrencia)
+) AS t1
+GROUP BY 
+    t1.tipoLocal, t1.ano, t1.mes
+ORDER BY 
+    t1.tipoLocal, t1.ano, t1.mes;
+
+
+SET lc_time_names = 'pt_BR';
+
+
